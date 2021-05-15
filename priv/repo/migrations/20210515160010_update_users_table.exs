ce078e3ae0c4b48ce5416fda5c9d302799d09e39
ThiagoPMaceda <thiagopmaceda@gmail.com>
2021-05-15 16:16:28 -0300

    update database migrations

--- priv/repo/migrations/20210515160010_update_users_table.exs
+++ priv/repo/migrations/20210515160010_update_users_table.exs
@@ -1,11 +1,12 @@
-defmodule Betrybebackendchallenge.Repo.Migrations.CreateUsers do
+defmodule Betrybebackendchallenge.Repo.Migrations.UpdateUsersTable do
   use Ecto.Migration
 
   def change do
     create table(:users) do
       add :displayname, :string
       add :email, :string
-      add :password, :string
+      add :password, :string, virtual: true
+      add :password_hash, :string
       add :image, :string
 
       timestamps()
