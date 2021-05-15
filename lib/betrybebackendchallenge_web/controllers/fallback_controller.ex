e082b4f422e193ca29493826a1d4299a7308c71b
ThiagoPMaceda <thiagopmaceda@gmail.com>
2021-05-15 16:17:50 -0300

    add new call pattern match when email or password dont match

--- lib/betrybebackendchallenge_web/controllers/fallback_controller.ex
+++ lib/betrybebackendchallenge_web/controllers/fallback_controller.ex
@@ -4,9 +4,18 @@ defmodule BetrybebackendchallengeWeb.FallbackController do
   alias BetrybebackendchallengeWeb.ErrorView
 
   def call(conn, {:error, %{result: result, status: status}}) do
+    IO.inspect(result)
+
     conn
     |> put_status(status)
     |> put_view(ErrorView)
     |> render("error.json", result: result)
+  end
+
+  def call(conn, {:error, :email_or_password_invalid}) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(ErrorView)
+    |> render("error.json", result: "Campos inválidos")
   end
 end
