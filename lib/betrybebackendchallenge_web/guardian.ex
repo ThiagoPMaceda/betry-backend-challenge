6977a237f342d511f9b8a05dab3327a221d3bd3f
ThiagoPMaceda <thiagopmaceda@gmail.com>
2021-05-15 23:23:03 -0300

    add methods to catch errors in guardian

--- lib/betrybebackendchallenge_web/guardian.ex
+++ lib/betrybebackendchallenge_web/guardian.ex
@@ -6,9 +6,17 @@ defmodule BetrybebackendchallengeWeb.Guardian do
     {:ok, sub}
   end
 
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
+
   def resource_from_claims(claims) do
     id = claims["sub"]
     resource = Betrybebackendchallenge.Users.get_user!(id)
     {:ok, resource}
+  end
+
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
   end
 end
