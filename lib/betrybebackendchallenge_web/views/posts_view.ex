0f28ba3e0685752d39a6240dfafd13b004e0b183
ThiagoPMaceda <thiagopmaceda@gmail.com>
2021-05-17 00:00:42 -0300

    add view to index, show and update

--- lib/betrybebackendchallenge_web/views/posts_view.ex
+++ lib/betrybebackendchallenge_web/views/posts_view.ex
@@ -8,4 +8,32 @@ defmodule BetrybebackendchallengeWeb.PostsView do
       userId: post.user_id
     }
   end
+
+  def render("index.json", %{posts_and_users: posts_and_users}) do
+    posts_and_users
+  end
+
+  def render("show.json", %{post: post}) do
+    %{
+      title: post.title,
+      id: post.id,
+      content: post.content,
+      published: post.published,
+      updated: post.updated,
+      user: %{
+        id: post.user.id,
+        displayName: post.user.displayname,
+        email: post.user.email,
+        image: post.user.image
+      }
+    }
+  end
+
+  def render("update.json", %{updated_post: updated_post}) do
+    %{
+      title: updated_post.title,
+      content: updated_post.content,
+      userId: updated_post.user_id
+    }
+  end
 end
