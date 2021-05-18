7431119ed014b87590a5a9325146ad59da4aebc0
ThiagoPMaceda <thiagopmaceda@gmail.com>
2021-05-18 13:13:05 -0300

    add search_post_by_term

--- lib/betrybebackendchallenge/blog.ex
+++ lib/betrybebackendchallenge/blog.ex
@@ -28,6 +28,15 @@ defmodule Betrybebackendchallenge.Blog do
     |> Repo.preload(:user)
   end
 
+  def search_post_by_term(term) do
+    Repo.all(
+      from p in Post,
+        where: ilike(p.title, ^"%#{term}%"),
+        or_where: ilike(p.content, ^"%#{term}%")
+    )
+    |> Repo.preload(:user)
+  end
+
   @doc """
   Gets a single post.
 
