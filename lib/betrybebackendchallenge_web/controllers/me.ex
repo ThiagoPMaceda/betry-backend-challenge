6aa7901a12130abc3534f5017f08370eb425f70b
ThiagoPMaceda <thiagopmaceda@gmail.com>
2021-05-18 13:13:34 -0300

    add alias to Plug and Users

--- lib/betrybebackendchallenge_web/controllers/me.ex
+++ lib/betrybebackendchallenge_web/controllers/me.ex
@@ -1,15 +1,16 @@
 defmodule BetrybebackendchallengeWeb.MeController do
   use BetrybebackendchallengeWeb, :controller
 
-  alias BetrybebackendchallengeWeb.Guardian
+  alias BetrybebackendchallengeWeb.Guardian.Plug
   alias Betrybebackendchallenge.Users.User
+  alias Betrybebackendchallenge.Users
   alias BetrybebackendchallengeWeb.FallbackController
 
   action_fallback FallbackController
 
   def delete(conn, _) do
-    with {:ok, %User{} = user} <- BetrybebackendchallengeWeb.Guardian.Plug.current_resource(conn) do
-      with {:ok, _} <- Betrybebackendchallenge.Users.delete_user(user) do
+    with {:ok, %User{} = user} <- Plug.current_resource(conn) do
+      with {:ok, _} <- Users.delete_user(user) do
         conn
         |> send_resp(:no_content, "")
       end
